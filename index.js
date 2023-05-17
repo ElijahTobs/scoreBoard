bc9cab1b23bc233c7e7f4976ad4bde12eb4f667f
Comfort Ige <eligenius@gmail.com>
2023-05-17 21:24:41 +0100

    Create variables for each node buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+const scoreBox = document.querySelectorAll(".score")
+for (x in scoreBox) {
+  scoreBox[x].textContent = 0
+}
+
+const home1 = document.getElementById("h-1")
+const home2 = document.getElementById("h-2")
+const home3 = document.getElementById("h-3")
+const guest1 = document.getElementById("g-1")
+const guest2 = document.getElementById("g-2")
+const guest3 = document.getElementById("g-3")
+
+
+
